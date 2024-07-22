15d12c5f46c068e30552f7a6aa3f7a0634f7fae9
Lucius Hebert <luciushebert13@gmail.com>
2024-07-22 08:47:40 -0300

    pr e push do footer final

--- src/components/footer/Contact.jsx
+++ src/components/footer/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             </div>
             
         </div>
-    )
+    );
 }
 
 export default Contact;
--- src/components/footer/Description.jsx
+++ src/components/footer/Description.jsx
@@ -3,11 +3,10 @@ import Logo from '../Logo'
 const Description = () => {
     return (
     <>
-    <div>
-        <Logo src='src\assets\logo-footer.svg'/>
-    </div>
+    <Logo src='src\assets\logo-footer.svg'/>
     <div className="mt-5">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
         </p>
     </div>   
     </>
