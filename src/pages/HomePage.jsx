d467a153c1f0ac3e2f6744d99db62a52e3af8d0b
rafaelcesar0 <rafaelcesar0@proton.me>
2024-07-23 11:38:14 -0300

    feat: add Gallery component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,23 @@
-import Layout from "../components/Layout"
-import Gallery from "../components/gallery/Gallery"
-import SlideImage from "../components/gallery/SlideImage"
+import Layout from '../components/Layout';
+import Gallery from '../components/Gallery';
 
 const HomePage = () => {
   return (
     <Layout>
-        <Gallery images={SlideImage}/>
+      <Gallery
+        slides={[
+          '/public/home-slide-1.jpeg',
+          '/public/home-slide-2.jpeg',
+          '/public/home-slide-3.jpeg',
+          '/public/home-slide-4.jpeg',
+          '/public/home-slide-5.jpeg',
+          '/public/home-slide-6.jpeg',
+          '/public/home-slide-7.jpeg',
+          '/public/home-slide-8.jpeg',
+        ]}
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
