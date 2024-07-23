0f4c9815fe22f2ec2f7ae02d095b6c0c0bde1321
rafaelcesar0 <rafaelcesar0@proton.me>
2024-07-23 11:58:19 -0300

    fix: tailwind bud sorted out

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-
-import withMT from "@material-tailwind/react/utils/withMT";
-
-module.exports = withMT({
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
